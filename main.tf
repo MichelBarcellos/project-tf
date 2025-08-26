42300c0b447ed998649a3113d47663411cc3d900
MichelBarcellos <msb2107@gmail.com>
2025-08-26 14:16:26 +0000

    teste

--- main.tf
+++ main.tf
@@ -29,3 +29,22 @@ output "instance_id" {
 output "instance_public_ip" {
   value = aws_instance.example.public_ip
 }
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.14.2"
+
+  name = "my-vpc"
+  cidr = "00101010101"
+  
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket-michel"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
