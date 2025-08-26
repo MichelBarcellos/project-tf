8c5d8189f14b42ee092b0b7c9fb0329831335eae
MichelBarcellos <msb2107@gmail.com>
2025-08-26 20:35:59 +0000

    security changes

--- main.tf
+++ main.tf
@@ -11,40 +11,44 @@ terraform {
 
 provider "aws" {
     region = "us-east-1"
-    profile = ""   
-  
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "example-instance"
-  }
-}
-output "instance_id" {
-  value = aws_instance.example.id
-}
-output "instance_public_ip" {
-  value = aws_instance.example.public_ip
-}
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
-
-  name = "my-vpc"
-  cidr = "00101010101"
+    profile = "default"   
   
 }
 
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket-michel"
-  acl    = "private"
+resource "aws_security_group" "terra-sg" {
+  name        = "terra-sg"
+  description = "regra sg projeto com tf"
+  vpc_id      = "vpc-026959c74313e105b"
+
+
+  ingress {
+    description = "Liberado para o mundo"
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+}
+}
+  
+  
+resource "aws_instance" "bia-dev" {
+  ami           = "ami-02f3f602d23f1659d"
+  instance_type = "t3.micro"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    ambiente = "dev"
+    Name = "instance-tf"
+    
   }
+  vpc_security_group_ids = [aws_security_group.terra-sg.id]
 }
