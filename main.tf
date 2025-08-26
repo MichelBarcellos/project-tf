433923d7f30bc09c556615690c83d0bdd063c399
MichelBarcellos <msb2107@gmail.com>
2025-08-26 13:56:31 +0000

    project-mod

--- main.tf
+++ main.tf
@@ -14,3 +14,17 @@ provider "aws" {
     profile = ""   
   
 }
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
+output "instance_id" {
+  value = aws_instance.example.id
+}
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
