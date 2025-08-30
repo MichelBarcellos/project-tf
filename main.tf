a9af7e87b79922ddd996cc3495ecce5fc00ab509
MichelBarcellos <msb2107@gmail.com>
2025-08-30 14:39:41 +0000

    state_config

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "terra-sg" {
 
 
   ingress {
-    description = "Liberado para o mundo2"
+    description = "Liberado para o mundo"
     from_port   = 3001
     to_port     = 3001
     protocol    = "tcp"
